ffdab5029c83d10c7c55fe25a0bfef0b618df069
Patrick Golden <ptgolden0@gmail.com>
2016-02-02 13:42:42 -0500

    Initial code to pull information from annotations

---
+++ README.md
@@ -0,0 +1,12 @@
+# Annotation tool
+
+A command-line tool to extract annotations from PDFs and convert them to the
+[Open Annotation Data Model](http://www.openannotation.org/spec/core/).
+
+## Requirements
+  * autotools
+  * libpoppler
+  * librdf
+
+## Building
+`./autogen.sh && ./configure && make`
---
+++ pdfannot.cc
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <goo/GooString.h>
+#include <PDFDoc.h>
+#include <PDFDocFactory.h>
+#include <Page.h>
+#include <Annot.h>
+#include <XRef.h>
+
+int main(int argv, char *args[]) {
+	PDFDoc *doc;
+	Page *page;
+
+	Annots *annots;
+	Annot *annot;
+
+	// Hard coded for now; in the future take from args
+	GooString *filename  = new GooString("/home/patrick/Code/projects/annot/test.pdf");
+
+	doc = PDFDocFactory().createPDFDoc(*filename, NULL, NULL);
+	int pages = doc->getNumPages();
+
+	page = doc->getPage(2);
+	annots = page->getAnnots();
+
+	for (int j = 0; j < annots->getNumAnnots(); ++j) {
+		annot = annots->getAnnot(j);
+
+		if (annot->getType() == Annot::typeStamp) {
+			AnnotStamp *stamp = static_cast<AnnotStamp *>(annot);
+			GooString *subject = stamp->getSubject();
+			printf("%s\n", subject->getCString());
+		}
+
+		if (annot->getType() == Annot::typeUnderline) {
+			AnnotTextMarkup *underline = static_cast<AnnotTextMarkup *>(annot);
+			Object annotObj;
+			Object obj1;
+
+			const Ref ref = annot->getRef();
+			doc->getXRef()->fetch(ref.num, ref.gen, &annotObj);
+
+			annotObj.dictLookup("MarkupText", &obj1);
+			printf("%s", obj1.getString()->getCString());
+
+			annotObj.free();
+			obj1.free();
+		}
+	}
+
+	delete filename;
+	delete doc;
+}
